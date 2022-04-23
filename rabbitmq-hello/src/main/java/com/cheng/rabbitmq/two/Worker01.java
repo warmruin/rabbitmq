3aa6097469b1ddcc681c3cea12289e4967104e94
Gong.Cheng <1349668699@qq.com>
2022-04-23 17:21:48 +0800

    工作队列 结果成功

--- rabbitmq-hello/src/main/java/com/cheng/rabbitmq/two/Worker01.java
+++ rabbitmq-hello/src/main/java/com/cheng/rabbitmq/two/Worker01.java
@@ -13,7 +13,7 @@ import com.rabbitmq.client.DeliverCallback;
  */
 public class Worker01 {
 
-    public static final String queue_name="hello";
+    public static final String QUEUE_NAME ="hello";
 //接收消息
     public static void main(String[] args) throws Exception {
 
@@ -27,6 +27,7 @@ public class Worker01 {
             System.out.println(consumerTag+"消息者取消消费接口回调逻辑");
         };
         //消息的接收
-        channel.basicConsume(queue_name,true,deliverCallback,cancelCallback);
+        System.out.println("C2等待接受消息....");
+        channel.basicConsume(QUEUE_NAME,true,deliverCallback,cancelCallback);
     }
 }
