bf028ae3ab9540e553eb66eeecc557812291f61a
Gong.Cheng <1349668699@qq.com>
2022-05-01 16:17:52 +0800

    Fanout 实战

---
+++ rabbitmq-hello/src/main/java/com/cheng/rabbitmq/five/EmitLog.java
@@ -0,0 +1,35 @@
+package com.cheng.rabbitmq.five;
+
+import com.cheng.rabbitmq.utils.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+
+import java.util.Scanner;
+
+/**
+ * EmitLog 发送消息给两个消费者接收
+ *
+ * @author Cris
+ * @date 2022/05/01
+ */
+public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        try (Channel channel = RabbitMqUtils.getChannel()) {
+/**
+ * 声明一个 exchange
+ * 1.exchange 的名称
+ * 2.exchange 的类型
+ */
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入信息");
+            while (sc.hasNext()) {
+                String message = sc.nextLine();
+                channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+                System.out.println("生产者发出消息" + message);
+            }
+        }
+
+    }
+}
